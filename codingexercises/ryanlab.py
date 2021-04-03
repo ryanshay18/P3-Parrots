eaf62dac84e14445a3f156893e4c5f45e2209ac3
ryanshay <ryanshay88@gmail.com>
2021-04-02 23:44:37 -0700

    minilab commit

---
+++ codingexercises/ryanlab.py
@@ -0,0 +1,28 @@
+
+class ryanLab:
+    def __init__(self, n):
+        self._n = n
+        self.primes = [2]
+        a = 2
+
+        while a < n:
+            counter = 0
+
+            for i in self.primes:
+                if a % i == 0:
+                    counter += 1
+
+            if counter == 0:
+                self.primes.append(a)
+            else:
+                counter = 0
+
+            a = a + 1
+
+    @property
+    def get_n(self):
+        return self._n
+
+    @property
+    def get_primes(self):
+        return self.primes
